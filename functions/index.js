ba6f370249e69026835a1f36e218d070797146a4
Hudson Pereira <hudson.byte@gmail.com>
2017-12-23 11:03:39 -0200

    🔐 authenticated routes

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,52 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+firebase.initializeApp(functions.config().firebase);
+
+const authenticate = (req, res, next) => {
+  const token = req.query.auth;
+
+ firebase.auth().verifyIdToken(token).then(user => {
+  req.user = user;
+  next();
+ }).catch(error => {
+   res.status(403).send('Unauthorized');
+ });
+};
+
+
+app.use(authenticate);
+
+app.post('/fight/:monsterId', (req, res) => {
+  console.log(req.params.monsterId);
+  firebase.database().ref('monsters/' + req.params.monsterId).once('value', snapshot => {
+    res.send({
+      user: req.user,
+      monsters: snapshot.val()
+    })
+  });
+});
+
+app.get('/me', (req, res) => {
+  res.send(req.user);
+});
+
+
+exports.api = functions.https.onRequest(app);
+
+exports.createProfile = functions.auth.user().onCreate(event => {
+  const user = event.data;
+
+  return firebase.database().ref(`profiles/${user.uid}`).set({
+    level: 1,
+    experience: 0,
+    current_hitpoints: 100,
+    max_hitpoints: 100,
+    current_stamina: 100,
+    max_stamina: 100,
+    gold: 100,
+    nickname: 'Andarilho Misterioso'
+  })
+});
